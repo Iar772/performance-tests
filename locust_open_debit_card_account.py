e5547f45de22ebe9cc235a6eb2ae6083403b2b56
Iaroslav Bulimov <iaroslavbulimov@192.168.100.4>
2026-01-29 20:34:48 +0300

    Практика: Применение HTTP API клиентов в нагрузочном сценарии

--- locust_open_debit_card_account.py
+++ locust_open_debit_card_account.py
@@ -1,33 +1,35 @@
-from locust import task, HttpUser, between
+from locust import task, User, between
 
-from tools.fakers import fake
+from clients.http.gateway.accounts.client import AccountsGatewayHTTPClient, build_accounts_gateway_locust_http_client
+from clients.http.gateway.users.client import UsersGatewayHTTPClient, build_users_gateway_locust_http_client
+from clients.http.gateway.users.schema import CreateUserResponseSchema
 
 
-class OpenDebitCardAccountScenarioUser(HttpUser):
+class OpenDebitCardAccountScenarioUser(User):
+    host = "localhost"
 
     wait_time = between(1, 3)
 
-    user_data: dict
+    users_gateway_client: UsersGatewayHTTPClient
+    accounts_gateway_client: AccountsGatewayHTTPClient
+    create_user_response: CreateUserResponseSchema
 
 
     def on_start(self) -> None:
+        """
+        Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
+        Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
+        """
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
+        self.accounts_gateway_client = build_accounts_gateway_locust_http_client(self.environment)
 
-        request = {
-            "email": fake.email(),
-            "lastName": fake.last_name(),
-            "firstName": fake.first_name(),
-            "middleName": fake.middle_name(),
-            "phoneNumber": fake.phone_number()
-        }
-        response = self.client.post("/api/v1/users", json=request)
-
-        self.user_data = response.json()
+        self.create_user_response = self.users_gateway_client.create_user()
 
     @task
     def open_debit_card_account(self) -> None:
-        request = {"userId": self.user_data['user']['id']}
-
-        self.client.post(
-            "/api/v1/accounts/open-debit-card-account",
-            json=request
-        )
+        """
+        Основная нагрузочная задача. Здесь мы открываем дебетовый аккаунт для пред-созданного пользователя.
+        POST запрос к /api/v1/accounts/open-debit-card-account.
+        :return:
+        """
+        self.accounts_gateway_client.open_debit_card_account(user_id=self.create_user_response.user.id)
