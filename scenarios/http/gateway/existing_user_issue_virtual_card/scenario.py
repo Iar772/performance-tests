1b9464afac05f314857c8c6a3116f6e7e57252af
Iaroslav Bulimov <iaroslavbulimov@192.168.100.4>
2026-02-05 18:17:55 +0300

    Практика: Выпуск виртуальной карты существующим пользователем

---
+++ scenarios/http/gateway/existing_user_issue_virtual_card/scenario.py
@@ -0,0 +1,52 @@
+from locust import task, events
+from locust.env import Environment
+
+from clients.http.gateway.locust import GatewayHTTPTaskSet
+from seeds.scenarios.existing_user_issue_virtual_card import ExistingUserIssueVirtualCardSeedsScenario
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
+
+    seeds_scenario.build()
+
+    environment.seeds = seeds_scenario.load()
+
+
+class IssueVirtualCardTaskSet(GatewayHTTPTaskSet):
+    seed_user: SeedUserResult
+
+    def on_start(self) -> None:
+        """
+        Получаем случайного пользователя
+        :return:
+        """
+        super().on_start()
+        self.seed_user = self.user.environment.seeds.get_random_user()
+
+    @task(4)
+    def get_accounts(self):
+        """
+        Получаем список счетов
+        :return:
+        """
+        self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
+
+    @task(1)
+    def issue_virtual_card(self):
+        """
+        Выпускаем виртуальную карту
+        :return:
+        """
+        self.cards_gateway_client.issue_virtual_card(
+            user_id=self.seed_user.user_id,
+            account_id=self.seed_user.debit_card_accounts[0].account_id,
+        )
+        # Поведение из условия: после выпуска юзер обычно обновляет список счетов
+        self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
+
+class IssueVirtualCardScenarioUser(LocustBaseUser):
+    tasks = [IssueVirtualCardTaskSet]
